362aa6291d06da6df66930c3a476cd694011251e
JODAHYE <57217119+JODAHYE@users.noreply.github.com>
2022-01-23 13:53:45 +0900

    skelton

--- src/containers/LoginContainer.js
+++ src/containers/LoginContainer.js
@@ -3,14 +3,16 @@ import { useDispatch, useSelector } from 'react-redux';
 import Login from '../components/Login';
 import { googleLogin, googleLogout } from '../modules/user';
 
-const LoginContainer = ({isLogin}) => {
+const LoginContainer = ({isLogin, setIsLogin}) => {
   const dispatch = useDispatch();
 
   const onGoogleLogin = () => {
     dispatch(googleLogin());
+    setIsLogin(true);
   }
   const onLogout = () => {
     dispatch(googleLogout());
+    setIsLogin(false);
   }
   return (
     <Login onLogout={onLogout} isLogin={isLogin} onGoogleLogin={onGoogleLogin}/>
